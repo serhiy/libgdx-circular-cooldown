afa9971f289997a979b98cf3e7605655f0c9eace
= <sboychen@PCTE23778.cern.ch>
2017-07-03 15:12:58 +0200

    Added project structure.

---
+++ core/src/xyz/codingdaddy/Main.java
@@ -0,0 +1,21 @@
+package xyz.codingdaddy;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Main extends ApplicationAdapter {
+
+	@Override
+	public void create () {
+		
+	}
+
+	@Override
+	public void render () {
+		
+	}
+	
+	@Override
+	public void dispose () {
+		
+	}
+}
---
+++ desktop/src/xyz/codingdaddy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package xyz.codingdaddy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import xyz.codingdaddy.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
